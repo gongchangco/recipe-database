eea710ac5b9477782e1f0b6b10d33d574de5fb0d
gongchangco <gongchie@gmail.com>
2022-12-20 13:47:03 -0500

    Add in random recipe

--- recipeDB/src/routes/about/+page.svelte
+++ recipeDB/src/routes/about/+page.svelte
@@ -1,4 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let count = 0;
+
+    function increment() {
+        count += 1;
+    }
+</script>
+
+<h1>Welcome to About</h1>
+<h3>Counting: {count}</h3>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<button>Click for Nothing</button>
+<button on:click={increment}>Click to Count</button>
